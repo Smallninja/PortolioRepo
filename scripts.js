9d04dc782fdd4a11e7c5abe3f778aa94281720e5
Smallninja <Smallninja.jay@gmail.com>
2024-01-10 17:08:00 -0600

    Added some console.logging stuff

---
+++ scripts.js
@@ -0,0 +1,9 @@
+/* Author: Joseph Gutierrez
+   Date: 01/10/2024
+*/
+
+var number1 = 1;
+number1 = number1 + 10;
+var number2 = prompt("Enter a Number!");
+
+console.log("The value for number1 is: " + number1 + "\nThe value for number2 is: " + number2);
