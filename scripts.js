6beb62799512fab6fcabc662da99fde65a10ffc0
Smallninja <Smallninja.jay@gmail.com>
2024-02-04 21:18:38 -0600

    changeBackgroundNStuff

--- scripts.js
+++ scripts.js
@@ -1,9 +1,27 @@
 /* Author: Joseph Gutierrez
    Date: 01/10/2024
 */
-let counter = 5;
-for(let i = 0; i <= counter; i++) {
-   if (i % 3 === 0 || i === 1) {
-      console.log(i);
+
+
+var container = document.getElementById('container');
+
+function addParagraph(){
+   for(var i = 0; i < 5; i++){
+      var newParagraph = document.createElement('p');
+      newParagraph.innerHTML = "This is the " + i + " p elment";
+      container.appendChild(newParagraph);
    }
+};
+addParagraph();
+
+function changeBackground(){
+   var backgroundBody = document.getElementsByTagName('body')[0];
+   console.log(backgroundBody);
+   backgroundBody.style.backgroundColor = "gray";
+   container.style.backgroundColor = "white";
 }
+
+var button = document.getElementById('changeColor');
+button.addEventListener("click", function(){
+   changeBackground();
+});
