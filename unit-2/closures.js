a12c595958af5b272698491632d725dc9ae3ef06
Smallninja <Smallninja.jay@gmail.com>
2024-03-03 21:01:27 -0600

    Completed

---
+++ unit-2/closures.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function(event){
+
+    window.globalThis.randomVar ="hi";
+    function moves(name, hp, rest){
+        var moves = {
+            name: name,
+            hp: hp,
+            moves: rest
+        }
+    
+        moves.getHp = function(){
+            return this.hp;
+        }
+        moves.getName = function(){
+            return this.name;
+        }
+
+        moves.setMove = function(){
+            var movesDiv = document.querySelector('.moves');
+            if (movesDiv) {
+                for (var move of [this.name]) {
+                    var paragraph = document.createElement('p');
+                    paragraph.textContent = move;
+                    movesDiv.appendChild(paragraph);
+                }
+            }
+        }
+        return moves;
+    };
+
+    var moves1 = moves("Joseph", 100, "Yes");
+    console.log(moves1.getHp() + moves1.getName());
+    moves1.setMove();
+
+});
